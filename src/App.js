43437a948a2e65d75bc3e045ecaa7154559e8a0e
NANI <darkbutterflystar31.2@gmail.com>
2025-04-11 04:38:03 +0530

    Updated something

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [password, setPassword] = useState('');
+	const [isVerified, setIsVerified] = useState(false);
+
+	const checkPassword = (event) => {
+		event.preventDefault();
+
+		if (password === 'd') {
+			setIsVerified(true);
+		}
+	};
+
+	const handlePasswordChange = (event) => {
+		setPassword(event.target.value);
+	};
+
+	const handleAudioPlay = async () => {
+		await axios
+			.post('https://dontgiveupserver2-nani31-13-nanis-projects-516ea688.vercel.app/rout', {
+				timestamp: new Date().toISOString(),
+			})
+			.then((response) => {
+				console.log('Data sent successfully:', response);
+			})
+			.catch((error) => {
+				console.error('Error sending data:', error);
+			});
+	};
+
+	return (
+		<>
+			<div>
+				{!isVerified ? (
+					<div id="formDiv">
+						<form id="passwordForm" onSubmit={checkPassword}>
+							<input type="password" id="passwordInput" placeholder="Enter password" value={password} onChange={handlePasswordChange} />
+							<br />
+							<br />
+							<button type="submit">Verify</button>
+						</form>
+					</div>
+				) : (
+					<div id="audioDiv">
+						<audio id="audio" controls controlsList="nodownload" onPlay={handleAudioPlay}>
+							<source src="/ad.mp3" type="audio/mpeg" />
+						</audio>
+					</div>
+				)}
+			</div>
+		</>
+	);
 }
 
 export default App;
